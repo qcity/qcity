4fab557db0a9a23525d47159ede441a0c762b876
etrasys <user@users-iMac.local>
2018-10-20 18:00:21 +0900

    pack

--- pack.sh
+++ pack.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 coin=qcity
-version=1.0.0
+version=0.14.5
 platform=`uname -a | awk '{print tolower($1)}'`
 arch=`uname -m`
 
 if test "${platform}" = "linux" ; then
     if test "${arch}" = "x86_64" ; then
-        tarball_name="bitcoin-${version}-linux64.tar.gz"
+        tarball_name="${coin}-${version}-linux64.tar.gz"
     elif test "${arch}" = "x86_32" ; then
-        tarball_name="bitcoin-${version}-linux32.tar.gz"
+        tarball_name="${coin}-${version}-linux32.tar.gz"
     fi
 elif test "${platform}" = "darwin" ; then
-    tarball_name="bitcoin-${version}-osx64.tar.gz"
+    tarball_name="${coin}-${version}-osx64.tar.gz"
 else
-    echo "Bitcoin binary distribution not available for platform and architecture"
+    echo "${coin} binary distribution not available for platform and architecture"
     exit -1
 fi
-bitcoin_dir=qcity-${version}
+bitcoin_dir=${coin}-${version}
 rm -Rf tmp/*
 rm -Rf ${tarball_name}
 
-mkdir -p tmp/${bitcoin_dir}/bin
+mkdir -p tmp/${coin}
 
-cp src/${coin}-cli          tmp/${bitcoin_dir}/bin/${coin}-cli
-cp src/${coin}-tx           tmp/${bitcoin_dir}/bin/${coin}-tx
-cp src/${coin}d             tmp/${bitcoin_dir}/bin/${coin}d
-cp src/test/test_bitcoin    tmp/${bitcoin_dir}/bin/test_${coin}
-cp src/wallet-utility       tmp/${bitcoin_dir}/bin/
-cp src/qt/${coin}-qt        tmp/${bitcoin_dir}/bin/${coin}-qt
-cp ${coin}.conf             tmp/${bitcoin_dir}/bin/${coin}.conf
+cp src/${coin}-cli          tmp/${coin}/${coin}-cli
+cp src/${coin}-tx           tmp/${coin}/${coin}-tx
+cp src/${coin}d             tmp/${coin}/${coin}d
+cp src/test/test_bitcoin    tmp/${coin}/test_${coin}
+cp src/wallet-utility       tmp/${coin}/
+cp src/qt/${coin}-qt        tmp/${coin}/${coin}-qt
+cp ${coin}.conf             tmp/${coin}/${coin}.conf
 cd tmp
 tar -zcvf ../${tarball_name} ./
 cd ../
